2778edf80e52bb71ed6b64dcc481259a3967ac94
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-01-22 13:54:52 +0200

    1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -8,6 +8,9 @@ const Stats=(props)=>{
     <p>Good: {props.good}</p>
     <p>Neutral: {props.neutral}</p>
     <p>Bad: {props.bad}</p>
+    <p>Total: {props.total}</p>
+    <p>Avarage: {props.average}</p>
+    <p>Positive: {props.positive}%</p>
     </>
   )
 }
@@ -25,20 +28,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal]=useState(0)
 
   const handleGoodClick=()=>{
     const updatedGood=good+1
     setGood(updatedGood)
+    setTotal(updatedGood+neutral+bad)
   }
 
   const handleNeutralClick=()=>{
     const updatedNeutral=neutral+1
     setNeutral(updatedNeutral)
+    setTotal(good+updatedNeutral+bad)
   }
 
   const handleBadClick=()=>{
     const updatedBad=bad+1
     setBad(updatedBad)
+    setTotal(good+neutral+updatedBad)
+  }
+
+  const countAverage=()=>{
+    if (total==0)return 0
+    else return (good*1+bad*-1)/total
+  }
+
+  const countPositive=()=>{
+    if (total==0)return 0
+    else return good/total*100
   }
 
   return (
@@ -48,7 +65,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text="Neutral"/>
       <Button handleClick={handleBadClick} text="Bad"/>
       <Header text="Statistics"/>
-      <Stats good={good} neutral={neutral} bad={bad}/>
+      <Stats good={good} neutral={neutral} bad={bad} total={total} average={countAverage()} positive={countPositive()}/>
     </div>
   )
 }
