55d5c81deed5b20cf4f850bf09862eca51ac7f8b
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-03-08 10:48:33 +0200

    4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter=require('./controllers/blogs')
 const usersRouter=require('./controllers/users')
+const loginRouter=require('./controllers/login')
 
 
 
@@ -17,6 +18,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
