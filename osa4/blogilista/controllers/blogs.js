65811598a421e97230c746829d0827cf5367ea77
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-03-10 16:06:59 +0200

    4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter=require('express').Router()
 const Blog=require('../models/blog')
 const User=require('../models/user')
+const jwt=require('jsonwebtoken')
 
 
 blogsRouter.get('/', async (request, response) => {
@@ -8,10 +9,22 @@ blogsRouter.get('/', async (request, response) => {
 	response.json(blogs)
 })
 
+const getTokenFrom=request=>{
+    const authorization=request.get('authorization')
+    if(authorization && authorization.startsWith('Bearer ')){
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
 	const body=request.body
+    const decodedToken=jwt.verify(getTokenFrom(request), process.env['SECRET'])
+    if(!decodedToken.id){
+        return response.status(401).json({error: 'invalid token'})
+    }
 
-	const user=await User.findById(body.userId)
+	const user=await User.findById(decodedToken.id)
 
 	const blog = new Blog(body)
 	if(!blog.likes){blog.likes=0}
--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -5,6 +5,9 @@ const errorHandler=(error, request, response, next) => {
 	else if(error.name==='MongoServerError' && error.message.includes('E11000 duplicate key error')){
 		return response.status(400).json({ error: 'excepted username to be unique' })
 	}
+    else if(error.name==='JsonWebTokenError'){
+        return response.status(400).json({error: 'token missing or invalid'})
+    }
 
 	next(error)
 }
