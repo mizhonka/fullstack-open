7cc0b773df9b11a343a212e9d1f04d0b97088dc3
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-03-14 16:41:45 +0200

    5.2

--- osa5/bloglist-frontend/src/App.jsx
+++ osa5/bloglist-frontend/src/App.jsx
@@ -5,55 +5,75 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername]=useState('')
-  const [password, setPassword]=useState('')
-  const [user, setUser]=useState(null)
+    const [blogs, setBlogs] = useState([])
+    const [username, setUsername]=useState('')
+    const [password, setPassword]=useState('')
+    const [user, setUser]=useState(null)
 
-  useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )
-  }, [])
+    const updateBlogs=()=>{
+        blogService.getAll().then(blogs =>
+            setBlogs( blogs )
+        )
+    }
 
-  const handleUsername=event=>setUsername(event.target.value)
-  const handlePassword=event=>setPassword(event.target.value)
+    useEffect(() => {
+        updateBlogs()
+    }, [])
 
-  const handleLogin=async event=>{
+    useEffect(()=>{
+    const loggedUserJSON=window.localStorage.getItem('loggedUser')
+    if(loggedUserJSON){
+        const user=JSON.parse(loggedUserJSON)
+        setUser(user)
+        blogService.setToken(user.token)
+        updateBlogs()
+    }
+    },[])
+
+    const handleUsername=event=>setUsername(event.target.value)
+    const handlePassword=event=>setPassword(event.target.value)
+
+    const logOut=()=>{
+        window.localStorage.removeItem('loggedUser')
+        setUser(null)
+    }
+
+    const handleLogin=async event=>{
         event.preventDefault()
 
         try{
             const user=await loginService.login({username, password})
             setUser(user)
             blogService.setToken(user.token)
-            blogService.getAll().then(blogs =>
-                setBlogs( blogs )
-              )
+            window.localStorage.setItem('loggedUser', JSON.stringify(user))
+            updateBlogs()
             setUsername('')
             setPassword('')
         } catch (exception) {
             console.log('wrong credentials')
-        }
-    }
+        }}
 
-  if(user===null){
+
+
+    if(user===null){
     return (
         <div>
             <h2>login to application:</h2>
             <Login handleLogin={handleLogin} username={username} handleUsername={handleUsername} password={password} handlePassword={handlePassword}/>
         </div>
     )
-  }
+    }
 
-  return (
+    return (
     <div>
-      <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        <button onClick={logOut}>logout</button>
+        {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
-      )}
+        )}
     </div>
-  )
+    )
 }
 
 export default App
