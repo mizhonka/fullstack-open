2ec9a388664d55448994c1dbb3878a8f2ce8ecf3
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-03-22 14:18:21 +0200

    6.9

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,16 @@ import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList=()=>{
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
+
+    const anecdotes = useSelector(state => {
+        const filterMatch=(anecdote)=>{
+            const filter=state['filter']
+            const reg=new RegExp(`\S*${filter}\S*`, 'i')
+            return reg.test(anecdote.content)
+        }
+
+        return state['anecdotes'].filter(filterMatch)
+    })
 
     const vote = (id) => {
         console.log('vote', id)
