118825d7f7a86e7a06b2a7d0a8c7102811ab5614
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-04-10 16:21:50 +0300

    8.4

--- osa8/index.js
+++ osa8/index.js
@@ -83,7 +83,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 
@@ -105,7 +105,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => {return books},
+    allBooks: (root, args) => {
+        if(!args.author){
+            return books
+        }
+        return books.filter(b=>b.author===args.author)
+    },
     allAuthors: ()=>{return authors}
   },
   Author: {
