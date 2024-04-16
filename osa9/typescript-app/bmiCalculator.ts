b36fb166c4130b7905587bb99fe7c984dda67dd7
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-04-16 13:53:53 +0300

    9.5

--- osa9/typescript-app/bmiCalculator.ts
+++ osa9/typescript-app/bmiCalculator.ts
@@ -1,46 +1,22 @@
-interface PersonData{
-    weight: number;
-    height: number;
-}
+const BMICalculator=(weight: number, height: number):string=>{
 
-const parseBmiArguments=(args: string[]):PersonData=>{
-    if (args.length < 4) throw new Error('Not enough arguments!');
-    if (args.length > 4) throw new Error('Too many arguments!');
+    const bmiCalculator = (height: number, weight: number):string=>{
+        const heightInM = height/100;
+        const bmi = weight/(heightInM*heightInM);
 
-    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
-        return {
-            weight: Number(args[3]),
-            height: Number(args[2])
+        if(bmi<18.5){
+            return 'Underweight';
         }
+        if(18.5 <= bmi && bmi<=24.9){
+            return 'Normal (healthy weight)';
+        }
+        if(25 <= bmi && bmi <= 29.9){
+            return 'Overweight';
+        }
+        return 'Obese';
     }
-    else {
-        throw new Error('Provided values were not numbers!');
-    }
+
+    return bmiCalculator(height, weight);
 }
 
-const bmiCalculator = (height: number, weight: number):string=>{
-    const heightInM = height/100;
-    const bmi = weight/(heightInM*heightInM);
-
-    if(bmi<18.5){
-        return 'Underweight';
-    }
-    if(18.5 <= bmi && bmi<=24.9){
-        return 'Normal (healthy weight)';
-    }
-    if(25 <= bmi && bmi <= 29.9){
-        return 'Overweight';
-    }
-    return 'Obese';
-}
-
-try {
-    const {weight, height} = parseBmiArguments(process.argv);
-    console.log(bmiCalculator(height, weight));
-} catch(error:unknown){
-    let errorMessage = 'Something bad happened. '
-    if(error instanceof Error){
-        errorMessage=errorMessage.concat(error.message)
-    }
-    console.log(errorMessage)
-}
+export default BMICalculator;
--- osa9/typescript-app/index.ts
+++ osa9/typescript-app/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import BMICalculator from './bmiCalculator';
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get('/bmi', (req, res) => {
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+    if(isNaN(weight) || isNaN(height)){
+        res.json({error: 'malformatted parameters'})
+        return;
+    }
+    const bmi = BMICalculator(weight, height);
+    res.json(
+        {
+            weight: weight,
+            height: height,
+            bmi: bmi
+        }
+    )
 });
 
 const PORT = 3003;
