8fad1b92f013721a5b6d65f7b442363b07eeccac
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-04-17 16:42:52 +0300

    9.14

---
+++ osa9/courses/src/components/Content.tsx
@@ -0,0 +1,12 @@
+interface ContentProps{
+    name: string,
+    exerciseCount: number
+}
+
+const Content=(props: ContentProps)=>{
+    return(
+        <p>{props.name} {props.exerciseCount}</p>
+    )
+}
+
+export default Content;
