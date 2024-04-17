e863d90903ba1460d37d359c1860236874df8904
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-04-17 10:11:08 +0300

    9.7

--- osa9/typescript-app/exerciseCalculator.ts
+++ osa9/typescript-app/exerciseCalculator.ts
@@ -1,3 +1,49 @@
+const ExerciseCalculator=(hours: number[], target: number):TrainingResults=>{
+    const getRating = (result: number, target: number):Rating=>{
+        if(result>=target){
+            return {
+                numeric: 3,
+                written: 'well done!'
+            };
+        }
+        if(result>=target/2){
+            return {
+                numeric: 2,
+                written: 'not too bad but could be better'
+            };
+        }
+        return {
+            numeric: 1,
+            written: 'not great'
+        };
+    };
+
+    const exerciseCalculator = (hours: number[], target: number):TrainingResults=>{
+        const hoursN = hours.map(h=>{
+            const hn = Number(h);
+            if(isNaN(hn)){
+                throw new Error('malformatted parameters');
+            }
+            return hn;
+        });
+        const daysTrained=hoursN.filter(h=>h>0);
+        const hourSum=daysTrained.reduce((total, current)=>total+current,0);
+        const average = hourSum / hours.length;
+        const result=getRating(average, target);
+        return {
+            periodLength: hours.length,
+            trainingDays: daysTrained.length,
+            success: average >= target,
+            rating: result.numeric,
+            ratingDescription: result.written,
+            target: target,
+            average: average
+        };
+    };
+
+    return exerciseCalculator(hours, target);
+};
+
 interface TrainingResults {
     periodLength: number;
     trainingDays: number;
@@ -13,75 +59,6 @@ interface Rating {
     written: string;
 }
 
-interface ExerciseArguments {
-    hours: number[];
-    target: number;
-}
 
-const parseExerciseArguments=(args: string[]):ExerciseArguments=>{
-    if (args.length < 4) throw new Error('Not enough arguments!');
-
-    const numeric = args.slice(2);
-    const hourArgs = numeric.slice(0, -1);
-    const hours = hourArgs.map(n => {
-        if(isNaN(Number(n))){
-            throw new Error('Provided values were not numbers!');
-        }
-        return Number(n);
-    });
-    const target = Number(numeric.slice(-1));
-    if(isNaN(target)){
-        throw new Error('Provided values were not numbers!');
-    }
-    return {
-        hours: hours,
-        target: target
-    };
-};
-
-const getRating = (result: number, target: number):Rating=>{
-    if(result>=target){
-        return {
-            numeric: 3,
-            written: 'well done!'
-        };
-    }
-    if(result>=target/2){
-        return {
-            numeric: 2,
-            written: 'not too bad but could be better'
-        };
-    }
-    return {
-        numeric: 1,
-        written: 'not great'
-    };
-};
-
-const exerciseCalculator = (hours: number[], target: number):TrainingResults=>{
-    const daysTrained=hours.filter(h=>h>0);
-    const hourSum=daysTrained.reduce((total, current)=>total+current,0);
-    const average = hourSum / hours.length;
-    const result=getRating(average, target);
-    return {
-        periodLength: hours.length,
-        trainingDays: daysTrained.length,
-        success: average >= target,
-        rating: result.numeric,
-        ratingDescription: result.written,
-        target: target,
-        average: average
-    };
-};
-
-try {
-    const {hours, target} = parseExerciseArguments(process.argv);
-    console.log(exerciseCalculator(hours, target));
-} catch(error:unknown){
-    let errorMessage = 'Something bad happened. ';
-    if(error instanceof Error){
-        errorMessage=errorMessage.concat(error.message);
-    }
-    console.log(errorMessage);
-}
+export default ExerciseCalculator;
 
--- osa9/typescript-app/index.ts
+++ osa9/typescript-app/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import BMICalculator from './bmiCalculator';
+import ExerciseCalculator from './exerciseCalculator';
 const app = express();
+app.use(express.json());
 
 app.get('/bmi', (req, res) => {
     const weight = Number(req.query.weight);
@@ -17,6 +19,27 @@ app.get('/bmi', (req, res) => {
             bmi: bmi
         }
     );
+});
+
+app.post('/exercises', (req, res)=>{
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const {daily_exercises, target} = req.body;
+    if(!daily_exercises || !target){
+        return res.status(400).json({error: 'parameters missing'});
+    }
+    if(isNaN(Number(target)) || !Array.isArray(daily_exercises)){
+        return res.status(400).json({error: 'malformatted parameters'});
+    }
+    try{
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        return res.status(201).json(ExerciseCalculator(daily_exercises, Number(target)));
+    }
+    catch(error:unknown){
+        if(error instanceof Error){
+            return res.status(400).json({error: String(error.message)});
+        }
+        return res.status(404).json({error: 'error unknown'});
+    }
 });
 
 const PORT = 3003;
