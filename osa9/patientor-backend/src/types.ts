7da4d7e9dc27869c66e12d59c1e8120828e1d7e3
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-04-22 09:08:48 +0300

    9.22

--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -1,7 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export interface Diagnosis{
     code: string;
     name: string;
@@ -24,5 +20,47 @@ export enum Gender{
     Other = 'other'
 }
 
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+
+export interface SickLeaveDates{
+    startDate: string;
+    endDate: string;
+}
+
+export interface Discharge{
+    date: string;
+    criteria: string;
+}
+
+export interface BaseEntry{
+    id: string;
+    date: string;
+    specialist: string;
+    description: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry{
+    type: 'HealthCheck';
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry{
+    type: 'OccupationalHealthcare';
+    employerName: string;
+    sickLeave?: SickLeaveDates;
+}
+
+interface HospitalEntry extends BaseEntry{
+    type: 'Hospital';
+    discharge: Discharge;
+}
+
 export type PatientSecure = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
+export type Entry = HealthCheckEntry | OccupationalHealthcareEntry | HospitalEntry;
